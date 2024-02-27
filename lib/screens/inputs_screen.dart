099b0b2396f196880c87209303f6ac56236dbcd6
juandelcar <juandelcar123c@gmail.com>
2024-02-27 14:01:51 -0600

    Adding Switch and Slider into InputsScreen

--- lib/screens/inputs_screen.dart
+++ lib/screens/inputs_screen.dart
@@ -9,28 +9,105 @@ class InputsScreen extends StatefulWidget {
 }
 
 class _InputsScreenState extends State<InputsScreen> {
+  bool valueSwitch = false;
+  double valueSlider = 0.0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Entradas"),),
+      appBar: AppBar(
+        title: const Text("Entradas"),
+      ),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
-          Text("Entradas",
-          style: AppTheme.lightTheme.textTheme.headlineLarge,),
-           const Row(
+          Text(
+            "Entradas",
+            style: AppTheme.lightTheme.textTheme.headlineLarge,
+          ),
+          entradaTexto(),
+          entradaSwitch(),
+          entradaSlider(),
+          const Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-              ElevatedButton(onPressed: null,
-               child: Text('Regresar', ),
-               ),
-               ElevatedButton(onPressed: null,
-               child: Text('Ir a Data Screen',),
-               )
+              ElevatedButton(
+                onPressed: null,
+                child: Text(
+                  'Regresar',
+                ),
+              ),
+              ElevatedButton(
+                onPressed: null,
+                child: Text(
+                  'Ir a Data Screen',
+                ),
+              )
             ],
           )
         ],
       ),
+    );
+  }
+
+  TextField entradaTexto() {
+    return TextField(
+      style: AppTheme.lightTheme.textTheme.headlineMedium,
+      decoration: InputDecoration(
+        border: const UnderlineInputBorder(),
+        labelText: "Escribe tu nombre:",
+        labelStyle: AppTheme.lightTheme.textTheme.headlineLarge,
+      ),
+    );
+  }
+
+  Row entradaSwitch() {
+    return Row(
+      // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget>[
+        Text(
+          '¿Te gusta Flutter?',
+          style: AppTheme.lightTheme.textTheme.headlineLarge,
+        ),
+        const SizedBox(
+          width: 25.0,
+        ),
+        Switch(
+          value: valueSwitch,
+          onChanged: (value) {
+            setState(() {
+              valueSwitch = value;
+              print('Estado del switch: $valueSwitch');
+            });
+          },
+        )
+      ],
+    );
+  }
+
+  Column entradaSlider() {
+    return Column(
+      children: [
+        Text(
+          '¿Que tanto te gusta flutter',
+          style: AppTheme.lightTheme.textTheme.headlineLarge,
+        ),
+        Slider(
+            min: 0.0,
+            max: 10.0,
+            value: valueSlider,
+            activeColor: AppTheme.mainColor,
+            inactiveColor: Colors.grey,
+            thumbColor: Colors.red,
+            label: '${valueSlider.round()}',
+            divisions: 10,
+            onChanged: (value) {
+              setState(() {
+                valueSlider = value;
+                print('Valor del slider: $valueSlider');
+              });
+            })
+      ],
     );
   }
 }
--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -4,6 +4,7 @@ import 'package:google_fonts/google_fonts.dart';
 class AppTheme {
   static const mainColor = Color.fromARGB(255, 29, 245, 245);
   static const backColor = Color.fromARGB(255, 234, 240, 240);
+  static const frontColor = Color.fromARGB(255, 71, 234, 210);
 
   // Declaraciones del tema claro
   static final ThemeData lightTheme = ThemeData().copyWith(
@@ -16,29 +17,29 @@ class AppTheme {
         fontWeight: FontWeight.bold,
       ),
     ),
-
-    iconTheme: const IconThemeData(
-      color: Colors.black,
-      size: 35.0
-    ),
+    iconTheme: const IconThemeData(color: Colors.black, size: 35.0),
     elevatedButtonTheme: ElevatedButtonThemeData(
-      style:ButtonStyle(
+      style: ButtonStyle(
         backgroundColor: MaterialStateProperty.all(mainColor),
         foregroundColor: MaterialStateProperty.all(Colors.white),
-        textStyle: MaterialStateProperty.all(GoogleFonts.pacifico(color: Colors.white,
-        fontSize: 22.0)),
-      ) ,
+        textStyle: MaterialStateProperty.all(
+            GoogleFonts.pacifico(color: Colors.white, fontSize: 22.0)),
+      ),
     ),
     textTheme: TextTheme(
       headlineLarge: GoogleFonts.tiltNeon(
         color: const Color.fromARGB(232, 20, 21, 21),
         fontSize: 25.0,
         fontWeight: FontWeight.w500,
-        decoration: TextDecoration.underline,
-        decorationColor: Colors.black,
-        decorationStyle: TextDecorationStyle.solid,
         decorationThickness: 1.3,
         fontStyle: FontStyle.italic,
+      ),
+      headlineMedium: GoogleFonts.tiltNeon(
+        color: const Color.fromARGB(232, 20, 21, 21),
+        fontSize: 21.5,
+        fontWeight: FontWeight.w500,
+        decorationStyle: TextDecorationStyle.solid,
+        decorationThickness: 1.3,
       ),
       bodySmall: GoogleFonts.montserrat(
         fontWeight: FontWeight.w300,
