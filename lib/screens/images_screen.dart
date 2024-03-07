d72d81b04826c9cd5920df31117b620e60d2a165
juandelcar <juandelcar123c@gmail.com>
2024-03-07 11:17:14 -0600

    Images 3

--- lib/screens/images_screen.dart
+++ lib/screens/images_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:practica3/theme/app_theme.dart';
+import 'package:transparent_image/transparent_image.dart';
 
 class ImagesScreen extends StatefulWidget {
   const ImagesScreen({super.key});
@@ -21,7 +22,7 @@ class _ImagesScreenState extends State<ImagesScreen> {
       body: ListView(
         children: [
           imageCard(),
-          
+          imageWeb(),
         ],
       ),
     );
@@ -29,14 +30,52 @@ class _ImagesScreenState extends State<ImagesScreen> {
 
   Card imageCard() {
     return Card(
-      shape: BeveledRectangleBorder(borderRadius: BorderRadius.circular(30)),
-      child: SizedBox(
-              height: 500,
-              width: 500,
-              child: Image(
-                image: AssetImage('assets/img/coche.jpg'),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+      margin: const EdgeInsets.all(20),
+      elevation: 10,
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(30),
+        child: Column(
+          children: [
+            const Image(
+              image: AssetImage('assets/img/coche.jpg'),
+            ),
+            Container(
+              padding: const EdgeInsets.all(10),
+              child: Text(
+                'Batimovil',
+                style: AppTheme.lightTheme.textTheme.headlineMedium,
+              ),
+            ),
+          ],
         ),
-      ), 
+      ),
+    );
+  }
+
+  Stack imageWeb() {
+    return Stack(
+      children: <Widget>[
+        const Center(
+            child: CircularProgressIndicator(
+              backgroundColor: Color.fromARGB(255, 19, 238, 201),
+              color: Color.fromARGB(194, 58, 251, 55),
+            ),
+        ),
+
+        Center(
+          child: SizedBox(
+            height: 450,
+            width: 450,
+            child: FadeInImage.memoryNetwork(
+            placeholder: kTransparentImage, 
+            image: 'https://thehappening.com/wp-content/uploads/2022/05/pugs-3.jpg'),
+            // Para Mostrar Una Imagen de la web
+            // Image.network(
+            //   ), 
+          ),
+        ),
+      ],
     );
   }
 }
